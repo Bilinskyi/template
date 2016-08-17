3815886abac62ceda738497085eed9ea9c08e371
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-08-18 00:19:49 +0300

    v5.0

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+minifycss = require('gulp-minify-css'),
+rename = require('gulp-rename'),
+browserSync = require('browser-sync').create(),
+autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+	})
+})
+
+gulp.task('sass', function () {
+	gulp.src('./sass/**/style.scss')
+	.pipe(sass({outputStyle: 'compact'}))
+	.pipe(autoprefixer('last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
+});
+
+
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+	gulp.watch('./sass/**/style.scss', ['sass']);
+// Reloads the browser whenever HTML or JS files change
+gulp.watch('./*.html', browserSync.reload); 
+gulp.watch('./js/**/*.js', browserSync.reload);
+});
