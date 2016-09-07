4175de80514dc3c3c7416f764232655e3c661b9e
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-09-07 10:44:36 +0300

    v3

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ sass = require('gulp-sass'),
 minifycss = require('gulp-minify-css'),
 rename = require('gulp-rename'),
 browserSync = require('browser-sync').create(),
+imageop = require('gulp-image-optimization'),
 autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browserSync', function() {
@@ -15,13 +16,21 @@ gulp.task('browserSync', function() {
 	})
 })
 
+gulp.task('images', function(cb) {
+  gulp.src(['dev/**/*.png','dev/**/*.jpg','dev/**/*.gif','dev/**/*.jpeg']).pipe(imageop({
+    optimizationLevel: 5,
+    progressive: true,
+    interlaced: true
+  })).pipe(gulp.dest('./')).on('end', cb).on('error', cb);
+});
+
 gulp.task('sass', function () {
 	return gulp.src('./sass/**/style.scss')
-	.pipe(sass().on('error', sass.logError))	
-	.pipe(autoprefixer('last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(minifycss())
-	.pipe(gulp.dest('css'))
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer('last 10 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+	// .pipe(rename({suffix: '.min'}))
+	// .pipe(minifycss())
+	.pipe(gulp.dest('./css'))
 	.pipe(browserSync.reload({
 		stream: true
 	}));
